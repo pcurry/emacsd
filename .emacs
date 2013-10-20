9840ec3ffda6ad5dd83bccb7c90b3a3bea885562
Paul Curry <pcurry@gmail.com>
2013-10-20 02:47:36 -0400

    Set environment variables manually
    The Emacs.app Coca compile does not pick up the right environment variables by default.
    This change includes code to set the PATH and exec-path prior to el-get running, so that el-get has
    access to the version control tools it needs.  Following that, we can use exec-path-from-shell to read
    the MANPATH and PYTHONPATH from a login shell.  This is needed to work around Apple disabling the environment.plist
    in OSX 10.8 (Mountain Lion).

--- .emacs
+++ .emacs
@@ -1,46 +1,106 @@
-;; Current pcurry .emacs on Nomi MB Air
+;;; package --- Summary
 
+;;; Commentary:
+;;; Current pcurry .emacs on Nomi MB Air
+
+;;; Code:
 (setq inhibit-startup-message t)                 ;no splash screen
 (setq inhibit-splash-screen t)                   ;Eliminate GNU splash screen
+
+;;; Function to return the PATH variable from a shell
+;;; initialize with my environment settings.
+
+
+;;; Imports my shell environment PATH
+
+(let ((path (shell-command-to-string ". /etc/profile; . ~/.bash_profile; echo -n $PATH")))
+  (setenv "PATH" path)
+  (setq exec-path
+        (append
+         (split-string-and-unquote path ":")
+         exec-path)))
 
 ;; el-get basic setup and autoload
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+(unless (require 'el-get nil t)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (let (el-get-master-branch)
+       (goto-char (point-max))
+       (eval-print-last-sexp)))))
 
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+;; (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
 (el-get 'sync)
+
+(when (memq window-system '(mac ns))
+  (require 'exec-path-from-shell)
+  (exec-path-from-shell-copy-envs '("PYTHONPATH" "MANPATH")))
 
 (load-theme 'wombat)
 
-(show-paren-mode t)
-(ido-mode)
-(setq-default show-trailing-whitespace t)
-(delete-selection-mode t)
-
-
-(defun refresh-file ()
+;; desktop mode (save buffers on exit)
+;; autosave buffers
+(require 'desktop)
+(desktop-save-mode 1)
+(defun my-desktop-save ()
   (interactive)
-  (revert-buffer t t t))
+  ;; Don't call desktop-save-in-desktop-dir, as it prints a message.
+  (if (eq (desktop-owner) (emacs-pid))
+      (desktop-save desktop-dirname)))
 
+;;; autosave hook
+(add-hook 'auto-save-hook 'my-desktop-save)
+
+;; Some configuration to automatic behavior.
+(delete-selection-mode t)
+(show-paren-mode t)
+
+;; Interactive do mode
+(require 'ido)
+(ido-mode t)
+
+;; set some behavioral defaults
+(setq-default indent-tabs-mode nil)  ;; never indent with tabs
+(setq-default show-trailing-whitespace t)
 
 ;; Auto mode hooks
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 (add-to-list 'auto-mode-alist '("\\.sls$" . yaml-mode))
 
+;; text mode hooks
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+;;enable flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;;enable linum mode
+(add-hook 'python-mode-hook 'linum-mode)
+
+;; python-mode hooks and keymap
+(add-hook 'python-mode-hook
+	  (lambda ()
+            (setq py-smart-indentation nil)
+            (setq py-indent-offset 4)
+            (setq indent-tabs-mode nil)
+	    (define-key python-mode-map [f8] 'python-pep8)))
+
+(add-hook 'python-mode-hook 'auto-complete-mode)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
 
 ;; sh-mode keymap
 (add-hook 'sh-mode-hook
 	  (lambda ()
 	    (define-key sh-mode-map "\C-c#" 'comment-region)))
 
+(defun refresh-file ()
+  (interactive)
+  (revert-buffer t t t))
+
 ;; Global function key bindings
 (global-set-key [f5] 'refresh-file)
+(global-set-key [f7] 'linum-mode)
 (global-set-key [f9] 'delete-trailing-whitespace)
 
 ;; Take off some of the training wheels.
--- .emacs.d/el-get/.status.el
+++ .emacs.d/el-get/.status.el
@@ -1,26 +1,91 @@
-((el-get status "installed" recipe
-	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :features el-get :info "." :load "el-get.el"))
+((auctex status "installed" recipe
+         (:name auctex :website "http://www.gnu.org/software/auctex/" :description "AUCTeX is an extensible package for writing and formatting TeX files in GNU Emacs and XEmacs. It supports many different TeX macro packages, including AMS-TeX, LaTeX, Texinfo, ConTeXt, and docTeX (dtx files)." :type git :module "auctex" :url "git://git.savannah.gnu.org/auctex.git" :build
+                `(("./autogen.sh")
+                  ("./configure" "--without-texmf-dir" ,(cond
+                                                         ((eq system-type 'darwin)
+                                                          "--with-lispdir=`pwd`")
+                                                         (t ""))
+                   ,(concat "--with-emacs=" el-get-emacs))
+                  "make")
+                :load-path
+                ("." "preview")
+                :load
+                ("tex-site.el" "preview/preview-latex.el")
+                :info "doc"))
+ (auto-complete status "installed" recipe
+                (:name auto-complete :website "https://github.com/auto-complete/auto-complete" :description "The most intelligent auto-completion extension." :type github :pkgname "auto-complete/auto-complete" :depends
+                       (popup fuzzy)))
+ (autopair status "installed" recipe
+           (:name autopair :website "https://github.com/capitaomorte/autopair" :description "Autopair is an extension to the Emacs text editor that automatically pairs braces and quotes." :type github :pkgname "capitaomorte/autopair" :features autopair))
+ (cl-lib status "installed" recipe
+         (:name cl-lib :builtin "24.3" :type elpa :description "Properly prefixed CL functions and macros" :url "http://elpa.gnu.org/packages/cl-lib.html"))
+ (ctable status "installed" recipe
+         (:name ctable :description "Table Component for elisp" :type github :pkgname "kiwanami/emacs-ctable"))
+ (dash status "installed" recipe
+       (:name dash :description "A modern list api for Emacs. No 'cl required." :type github :pkgname "magnars/dash.el"))
+ (deferred status "installed" recipe
+   (:name deferred :description "Simple asynchronous functions for emacs lisp" :website "https://github.com/kiwanami/emacs-deferred" :type github :pkgname "kiwanami/emacs-deferred" :features "deferred"))
+ (el-get status "installed" recipe
+         (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (epc status "installed" recipe
+      (:name epc :description "An RPC stack for Emacs Lisp" :type github :pkgname "kiwanami/emacs-epc" :depends
+             (deferred ctable)))
+ (exec-path-from-shell status "installed" recipe
+                       (:name exec-path-from-shell :website "https://github.com/purcell/exec-path-from-shell" :description "Emacs plugin for dynamic PATH loading" :type github :pkgname "purcell/exec-path-from-shell"))
+ (f status "installed" recipe
+    (:name f :website "https://github.com/rejeep/f.el" :description "Modern API for working with files and directories in Emacs" :type github :pkgname "rejeep/f.el"))
+ (flycheck status "installed" recipe
+           (:name flycheck :type github :pkgname "lunaryorn/flycheck" :description "On-the-fly syntax checking extension" :build
+                  ("cd doc && makeinfo flycheck.texi")
+                  :info "./doc" :depends
+                  (s dash cl-lib f pkg-info)))
+ (fuzzy status "installed" recipe
+        (:name fuzzy :website "https://github.com/auto-complete/fuzzy-el" :description "Fuzzy matching utilities for GNU Emacs" :type github :pkgname "auto-complete/fuzzy-el"))
+ (haskell-mode status "installed" recipe
+               (:name haskell-mode :description "A Haskell editing mode" :type github :pkgname "haskell/haskell-mode" :info "." :build
+                      `(("make" ,(format "EMACS=%s" el-get-emacs)
+                         "all"))
+                      :post-init
+                      (progn
+                        (require 'haskell-mode-autoloads)
+                        (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+                        (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation))))
+ (jedi status "installed" recipe
+       (:name jedi :description "An awesome Python auto-completion for Emacs" :type github :pkgname "tkf/emacs-jedi" :build
+              (("make" "requirements"))
+              :build/windows-nt
+              (("make" "requirements" "PYTHON=python.exe" "BINDIR=Scripts"))
+              :build/berkeley-unix
+              (("gmake" "requirements"))
+              :submodule nil :depends
+              (epc auto-complete)))
  (markdown-mode status "installed" recipe
-		(:name markdown-mode :description "Major mode to edit Markdown files in Emacs" :type git :url "git://jblevins.org/git/markdown-mode.git" :post-init
-		       (add-to-list 'auto-mode-alist
-				    '("\\.\\(md\\|mdown\\|markdown\\)\\'" . markdown-mode))))
- (python status "installed" recipe
-	 (:name python :description "Python's flying circus support for Emacs" :type github :pkgname "fgallina/python.el"))
+                (:name markdown-mode :description "Major mode to edit Markdown files in Emacs" :website "http://jblevins.org/projects/markdown-mode/" :type git :url "git://jblevins.org/git/markdown-mode.git" :before
+                       (add-to-list 'auto-mode-alist
+                                    '("\\.\\(md\\|mdown\\|markdown\\)\\'" . markdown-mode))))
+ (pkg-info status "installed" recipe
+           (:name pkg-info :description "Provide information about Emacs packages." :type github :pkgname "lunaryorn/pkg-info.el" :depends s))
+ (popup status "installed" recipe
+        (:name popup :website "https://github.com/auto-complete/popup-el" :description "Visual Popup Interface Library for Emacs" :type github :pkgname "auto-complete/popup-el"))
  (python-mode status "installed" recipe
-	      (:type github :username "emacsmirror" :name python-mode :type emacsmirror :description "Major mode for editing Python programs" :features
-		     (python-mode doctest-mode)
-		     :compile nil :load "test/doctest-mode.el" :prepare
-		     (progn
-		       (autoload 'python-mode "python-mode" "Python editing mode." t)
-		       (add-to-list 'auto-mode-alist
-				    '("\\.py$" . python-mode))
-		       (add-to-list 'interpreter-mode-alist
-				    '("python" . python-mode)))))
+              (:name python-mode :description "Major mode for editing Python programs" :type bzr :url "lp:python-mode" :load-path
+                     ("." "test")
+                     :compile nil :prepare
+                     (progn
+                       (autoload 'python-mode "python-mode" "Python editing mode." t)
+                       (autoload 'doctest-mode "doctest-mode" "Doctest unittest editing mode." t)
+                       (setq py-install-directory
+                             (el-get-package-directory "python-mode"))
+                       (add-to-list 'auto-mode-alist
+                                    '("\\.py$" . python-mode))
+                       (add-to-list 'interpreter-mode-alist
+                                    '("python" . python-mode)))))
+ (python-pep8 status "installed" recipe
+              (:type github :pkgname "emacsmirror/python-pep8" :name python-pep8 :type emacsmirror :description "Minor mode for running `pep8'" :features python-pep8 :post-init
+                     (require 'tramp)))
  (rst-mode status "installed" recipe
-	   (:name rst-mode :description "Mode for viewing and editing reStructuredText-documents." :type http :url "http://docutils.sourceforge.net/tools/editors/emacs/rst.el" :features rst))
+           (:name rst-mode :description "Mode for viewing and editing reStructuredText-documents." :type http :url "http://docutils.sourceforge.net/tools/editors/emacs/rst.el" :features rst))
+ (s status "installed" recipe
+    (:name s :description "The long lost Emacs string manipulation library." :type github :pkgname "magnars/s.el" :features s))
  (yaml-mode status "installed" recipe
-	    (:name yaml-mode :description "Simple major mode to edit YAML file for emacs" :type github :pkgname "yoshiki/yaml-mode" :prepare
-		   (progn
-		     (autoload 'yaml-mode "yaml-mode" nil t)
-		     (add-to-list 'auto-mode-alist
-				  '("\\.ya?ml\\'" . yaml-mode))))))
+            (:name yaml-mode :description "Simple major mode to edit YAML file for emacs" :type github :pkgname "yoshiki/yaml-mode")))
